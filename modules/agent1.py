9adcdfec4eab7421b65875e13d5e352f59a0ace5
Ujjwal <Ujjwal22154010@akgec.ac.in>
2024-11-17 14:03:00 +0530

    groq agent implemented

--- modules/agent1.py
+++ modules/agent1.py
@@ -1,23 +1,32 @@
-import streamlit as st
 import os
-from langchain_groq import ChatGroq
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import create_retrieval_chain
-from langchain_community.vectorstores import FAISS
-from transformers import AutoTokenizer, AutoModel
-import torch
-
+from groq import Groq
 from dotenv import load_dotenv
 
+load_dotenv()
+groq_token=os.getenv("GROQ_KEY")
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-model = AutoModel.from_pretrained("bert-base-uncased")
 
-inputs = tokenizer("This is a sample sentence.", return_tensors="pt")
-outputs = model(**inputs)
-embedding = outputs.last_hidden_state
-sentence_embedding = embedding.mean(dim=1)  # Pool over tokens
-print("Sentence embedding shape:", sentence_embedding.shape)
 
+client=Groq(api_key=groq_token)
+
+chat_completion=client.chat.completions.create(
+    messages=[
+        {
+            "role":"system",
+            "content":"You are an assistant that helps with rephrasing and clarifying information."
+                  "The user has provided the following answer:"
+
+                  "Please rephrase this information in a concise and clear manner."
+        },
+        {
+            "role":"user",
+            "content":"explain llm",
+        }
+    ],
+    model="mixtral-8x7b-32768",
+    temperature=0.7,
+    max_tokens=256,
+    
+)
+
+print(chat_completion.choices[0].message.content)
